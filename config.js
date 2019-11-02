e5785b74bc2335eb2570cd55739e5470b8de9efe
david <senteix2@gmail.com>
2019-11-02 16:15:30 -0500

    removing files from root directory , they are already in asset folder

--- config.js
+++
@@ -1,5 +0,0 @@
-application={
-	database: "flashcardApp", 
-	flashcard: {store:"flashcard", index: { column: "question" , unique: true }},
-	config: {store:"config", index: { column: "sheet" , unique: true }}
-}
